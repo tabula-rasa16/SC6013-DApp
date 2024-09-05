df3320025b81426d65c8bd7b0b252e39a59be2a2
tabula-rasa16 <84778000+tabula-rasa16@users.noreply.github.com>
2024-09-05 12:42:08 +0000

    update

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask,render_template,request
+import sqlite3
+import datetime
 
 app=Flask(__name__)
 
 @app.route('/',methods=["get","post"])
 def index():
     return render_template('index.html')
+
+@app.route('/main',methods=["get","post"])
+def main():
+    r = request.form.get("q")
+    current_time = datetime.datetime.now()
+    conn = sqlite3.connect('dapp.db')
+    c = conn.cursor()
+    c.execute("insert into user values (?, ?)", (name, current_time))
+    conn.commit()
+    c.close()
+    conn.close()
+    return render_template('main.html',r=r)
 
 @app.route('/store_money',methods=["get","post"])
 def store_money():
